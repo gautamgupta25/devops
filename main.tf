54290b36acd84d6f66fefaf11ee1f90beba58c0a
gautam <gautam.gupta25@live.com>
2023-10-07 18:20:11 +0530

    updated RG information from Dharam

--- main.tf
+++ main.tf
@@ -1,18 +1,21 @@
 terraform {
+  required_version = ">= 1.0.0"
   required_providers {
     azurerm = {
       source = "hashicorp/azurerm"
-      version = "3.75.0"
+      version = ">= 2.0"
     }
   }
 }
 
 provider "azurerm" {
+   features {}
 }
 
-resource "azurerm_resource_group" "test_rg" {
-  name     = "Test_RG"
-  location = "West Europe"
+resource "azurerm_resource_group" "sap-rg" 
+{
+  name     = "ind-sap-rg-001"
+  location = "North Europe"
   # Comment From Dharam
 }
   # Configuration options
